6f209f4b0357b4ca669a3bc3b81b2fcd4fc85b8e
Syed <syed@fahmpartners.com>
2017-04-11 15:21:01 +0800

    Initial Commit

---
+++ tp-system-client/src/main/java/com/training2/main/MainTpSystemClient.java
@@ -0,0 +1,12 @@
+package com.training2.main;
+
+import org.apache.commons.lang.StringUtils;
+
+public class MainTpSystemClient {
+
+	public static void main(String[] args) {
+		
+		// Able to get apache lang class StringUtils
+		String str = StringUtils.EMPTY;
+	}
+}
